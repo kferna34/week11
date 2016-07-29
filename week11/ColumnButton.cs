88e411b7c19e24e315402d887b4953f5975f05e0
kferna34 <kferna34@my.centennialcollege.ca>
2016-07-29 10:40:24 -0400

    Add project files.

---
+++ week11/ColumnButton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace week11
+{
+    public enum ColumnButton
+    {
+        Details = 4,
+        Edit,
+        Delete
+    }
+}
